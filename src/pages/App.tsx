4084a95f16d5afd228ae057c1973419ea0384c3a
joaoth <joaopedromcj9@gmail.com>
2023-06-30 17:10:17 -0300

    components

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,10 +2,60 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../styles/index.css';
 import { Pagination } from '@mui/material';
+import Card from '../components/card/card'
 
 const App = () => {
-  
+  const [message, setMessage] = useState('');
+  const [values, setValues] = useState<any>();
+  const [listGames, setListGames] = useState<any>([])
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = listGames.slice(indexOfFirstItem, indexOfLastItem);
 
+  const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page);
+  };
+
+  useEffect(() => {
+    fetch('http://localhost:3001')
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/").then((response) => {
+      setListGames(response.data)
+    })
+  }, [])
+
+  const handleRegisterGame = () => {
+    axios.post("http://localhost:3001/games", {
+      name: values.name,
+      cost: values.cost,
+      category: values.category,
+    }).then((response) => {
+        setListGames([
+          ...listGames,
+          {
+            id: response.data[0].id,
+            name: values.name,
+            cost: values.cost,
+            category: values.category,
+          },
+        ]);
+      });
+  };
+
+  const handleChangeValues = (value: any) => {
+    setValues((prevValue: any) => ({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+
+    }))
+  }
 
   return (
     <div className="app-container">
@@ -18,9 +68,6 @@ const App = () => {
         <input className='register-input' type="text" placeholder="Categoria" name="category" onChange={handleChangeValues} />
         <button className="register-button" onClick={handleRegisterGame}>Cadastrar</button>
       </div>
-
-
-      <input type="text" onChange={handleChangeValues} /> <button onClick={handleSearchGames}>VAI</button>
 
       {currentItems.map((val: any) => (
         <Card
